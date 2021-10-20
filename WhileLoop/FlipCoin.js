58a4c573b8f0f754688f6af49bd53265dc4bf06f
nandinibaride <nandinibaride1234@gmail.com>
2021-10-20 18:52:16 +0530

    Add:Whileloop problems on the bascic of javascript

---
+++ WhileLoop/FlipCoin.js
@@ -0,0 +1,27 @@
+/*
+@purpose: Flip Coin till either Heads or Tails wins 11 times.
+@authour: Nandini.B
+*/
+let headCount = 0;
+let tailCount = 0;
+
+while (headCount != 11 && tailCount != 11) 
+{
+    let coin = Math.floor(Math.random() * 2);
+    if (coin == 0) 
+    {
+        headCount++;
+    }
+    else 
+    {
+        tailCount++;
+    }
+}
+if (headCount > tailCount) 
+{
+    console.log("Head Wins!\nHead Count : " + headCount + "\nTails Count : "+ tailCount);
+}
+else 
+{
+    console.log("Tail Wins!\nHead Count : " + headCount + "\nTails Count : "+ tailCount);
+}
